1504912b9c77c0fa70a632957faf66d846fcdd67
sivertheisholt <sivert.b.heisholt@protonmail.com>
2023-10-23 12:25:28 +0200

    First commit

---
+++ HttpTrigger1/__init__.py
@@ -0,0 +1,12 @@
+import logging
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    
+    return func.HttpResponse(
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200
+    )
