ec86e2410884ad0e6e4f38062d06140e985236fb
sivertheisholt <sivert.b.heisholt@protonmail.com>
2023-10-23 13:59:27 +0200

    Testing img detection

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -1,6 +1,7 @@
 import logging
 
 import azure.functions as func
+from HttpTrigger1.tblite_detection_img import detect_image
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -8,19 +9,14 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         req_body = req.get_json()
         if req_body:
             # Your code to process the POST data
-            print(f"Received data: {req_body}")
+            print(f"Received data")
 
             img_base64 = req_body['imgBase64']
 
-            return func.HttpResponse(img_base64, mimetype="text/plain")
+            res = detect_image(img_base64)
+
+            return func.HttpResponse(res, mimetype="text/plain")
         else:
             return func.HttpResponse("No data received", status_code=400)
     except Exception as e:
         return func.HttpResponse(str(e), status_code=500)
-
-    logging.info('Python HTTP trigger function processed a request.')
-
-    return func.HttpResponse(
-        "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-        status_code=200
-    )
