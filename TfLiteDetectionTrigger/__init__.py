c384d20a24590aadcf4def1c65bc2395e774284a
sivertheisholt <sivert.b.heisholt@protonmail.com>
2023-10-24 13:14:08 +0200

    Send data

--- TfLiteDetectionTrigger/__init__.py
+++ TfLiteDetectionTrigger/__init__.py
@@ -2,6 +2,7 @@ import logging
 
 import azure.functions as func
 from TfLiteDetectionTrigger.tflite_detection_img import detect_image
+import requests
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -14,6 +15,16 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             img_base64 = req_body['imgBase64']
 
             res = detect_image(img_base64)
+
+            url = 'https://bouvet-shenanigans.azurewebsites.net/tf'
+
+            response = requests.post(url, json=res)
+
+            if response.status_code == 200:
+                print("Successfully sent data to web app")
+            else:
+                print(
+                    f"Request failed with status code: {response.status_code}")
 
             return func.HttpResponse("Data processed successfully", status_code=200)
         else:
